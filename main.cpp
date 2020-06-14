3e264097d70d7f62e7266016095f94340d6e2284
tom-selleck <66917773+tom-selleck@users.noreply.github.com>
2020-06-14 21:12:39 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main()
+{
+    string hexString = "FF";
+    int decimal=0;
+    int len = hexString.size();
+    char arr[len];
+    int pow = len--;
+    int s=1;
+    strcpy(arr,hexString.c_str());
+    for(int i=0; i<len; i++)
+    {
+        while(pow--)
+        {
+            s*=16;
+        };
+        cout <<"arr [i]: " << arr[i] << endl;
+        cout << "s: " << s << endl;
+        if(arr[i]>=97&&arr[i]<=102){
+            decimal = decimal + arr[i]*s - 87;
+        }
+        else if(arr[i]>=65&&arr[i]<=70){
+            decimal = decimal + arr[i]*s - 55;
+        }
+        else{
+        decimal = decimal +((arr[i]-48)*s);
+        }
+    };
+    cout << "dec= " << decimal << endl;
+    return 0;
+}

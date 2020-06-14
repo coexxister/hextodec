a454edd4aac4778406fbeac83932b0009b318c9a
Grzegorz Debicki <debicki.grzegorz@outlook.com>
2020-06-14 20:50:27 +0100

    Changes in comments

--- main.cpp
+++ main.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 int main()
 {
-    //test
     string hexString = "FF";
-    int decimal=0;
-    int len = hexString.size();
-    char arr[len] = hexString.toCharArray();
-    int pow = len--;
-    int s=1;
-    for(int i=0; i<len; i++)
+    int len = hexString.length();
+
+    //variable in brackets must be constant
+    char arr[len];
+
+    for (int i = 0; i < arr.length(); i++) {
+        arr[0] = hexString[0];
+    }
+
+    int pow = arr.length() - 1;
+    int s = 1;
+    int decimal = 0;
+
+
+    for (int i = 0; i < len; i++)
     {
-        while(pow--)
+        pow--;
+        while (pow > 0)
         {
-            s*=16;
+            s *= 16;
         };
-        cout <<"arr [i]: " << arr[i] << endl;
+        cout << "arr [i]: " << arr[i] << endl;
         cout << "s: " << s << endl;
-        if(arr[i]>=97&&arr[i]<=102){
-            decimal = decimal + arr[i]*s - 87;
+        //lower case letters
+        if (arr[i] >= 97 && arr[i] <= 102) {
+            decimal = decimal + arr[i] * s - 87;
         }
-        else if(arr[i]>=65&&arr[i]<=70){
-            decimal = decimal + arr[i]*s - 55;
+        //upper case letters
+        else if (arr[i] >= 65 && arr[i] <= 70) {
+            decimal = decimal + arr[i] * s - 55;
         }
-        else{
-        decimal = decimal +((arr[i]-48)*s);
+        //number
+        else {
+            decimal = decimal + ((arr[i] - 48) * s);
         }
     };
     cout << "dec= " << decimal << endl;
